3be9e7d5f37a43fa66947959fe7910571c2cabe1
jianywu <wujianyue000@gmail.com>
2021-10-05 20:34:35 +0800

    Add step 1,2,3,4

--- httpserver/main.go
+++ httpserver/main.go
@@ -6,27 +6,20 @@ import (
 	"io"
 	"log"
 	"net/http"
-
-	"net/http/pprof"
-	_ "net/http/pprof"
+	"os"
 
 	"github.com/golang/glog"
+	"github.com/thinkeridea/go-extend/exnet"
 )
 
 func main() {
 	flag.Set("v", "4")
 	glog.V(2).Info("Starting http server...")
 	http.HandleFunc("/", rootHandler)
-	c, python, java := true, false, "no!"
-	fmt.Println(c, python, java)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
 	mux.HandleFunc("/healthz", healthz)
-	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	mux.HandleFunc("/headers", headers)
+	// mux.HandleFunc("/headers", headers)
 	err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatal(err)
@@ -34,26 +27,37 @@ func main() {
 
 }
 
-func headers(w http.ResponseWriter, r *http.Request) {
+func copyHeader(w http.ResponseWriter, r *http.Request) {
 	for name, headers := range r.Header {
 		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
+			// fmt.Fprintf(w, "%v: %v\n", name, h)
+			w.Header().Set(name, h)
 		}
 	}
 }
 
+// 4. 当访问 localhost/healthz 时，应返回200
 func healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "ok\n")
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("entering root handler")
-	user := r.URL.Query().Get("user")
-	if user != "" {
-		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
-	} else {
-		io.WriteString(w, "hello [stranger]\n")
+	// 1. 接收客户端 request，并将 request 中带的 header 写入 response header
+	copyHeader(w, r)
+	// 2. 读取当前系统的环境变量中的 VERSION 配置，并写入 response header
+	os.Setenv("VERSION", "0.0.1")
+	ver := os.Getenv("VERSION")
+	w.Header().Set("version", ver)
+	// 3. Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
+	ip := exnet.ClientPublicIP(r)
+	if ip == "" {
+		ip = exnet.ClientIP(r)
 	}
+
+	fmt.Println("Client IP is:", ip)
+	fmt.Println("Status code is:", w.Header().Get("Status-Code"))
+
 	io.WriteString(w, "===================Details of the http request header:============\n")
 	for k, v := range r.Header {
 		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
