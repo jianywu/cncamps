c15a9467cd683df351981e8c4c66b2f4925a0a84
Wu Jianyue <wujianyue000@gmail.com>
2021-12-08 15:34:53 +0800

    Add metrics, refer to cncamps in github.

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,62 +1,85 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
-	"log"
+	"math/rand"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/golang/glog"
-	"github.com/thinkeridea/go-extend/exnet"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/cncamps/httpserver/metrics"
 )
 
 func main() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt)
 	flag.Set("v", "4")
 	glog.V(2).Info("Starting http server...")
-	http.HandleFunc("/", rootHandler)
+	metrics.Register()
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/hello", rootHandler)
 	mux.HandleFunc("/healthz", healthz)
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
-	<-c
-	log.Fatal("Program interrupted")
-}
+	mux.Handle("/metrics", promhttp.Handler())
 
-func copyHeader(w http.ResponseWriter, r *http.Request) {
-	for name, headers := range r.Header {
-		for _, h := range headers {
-			w.Header().Set(name, h)
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			glog.Fatalf("listen: %s\n", err)
 		}
+	}()
+	glog.Info("Server Started")
+	<-done
+	glog.Info("Server Stopped")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// extra handling here
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		glog.Fatalf("Server Shutdown Failed:%+v", err)
 	}
+	glog.Info("Server Exited Properly")
 }
 
-// 4. 当访问 localhost/healthz 时，应返回200
 func healthz(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "ok\n")
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	// 1. 接收客户端 request，并将 request 中带的 header 写入 response header
-	copyHeader(w, r)
-	// 2. 读取当前系统的环境变量中的 VERSION 配置，并写入 response header
-	os.Setenv("VERSION", "0.0.1")
-	ver := os.Getenv("VERSION")
-	w.Header().Set("version", ver)
-	// 3. Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
-	ip := exnet.ClientPublicIP(r)
-	if ip == "" {
-		ip = exnet.ClientIP(r)
-	}
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
+}
 
-	fmt.Println("Client IP is:", ip)
-	fmt.Println("Status code is:", w.Header().Get("Status-Code"))
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	glog.V(4).Info("entering root handler")
+	timer := metrics.NewTimer()
+	defer timer.ObserveTotal()
+	user := r.URL.Query().Get("user")
+	delay := randInt(10, 2000)
+	time.Sleep(time.Millisecond * time.Duration(delay))
+	if user != "" {
+		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	} else {
+		io.WriteString(w, "hello [stranger]\n")
+	}
+	io.WriteString(w, "===================Details of the http request header:============\n")
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+	glog.V(4).Infof("Respond in %d ms", delay)
 }
